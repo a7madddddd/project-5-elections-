02efff47bed9b3ddec830d0ae30a831a26ee836f
a7madddddd <142698638+a7madddddd@users.noreply.github.com>
2024-08-06 10:52:12 +0300

    secound edit

---
+++ SUHA/fstCopy_Proj5/fstCopy_Proj5/Controllers/DATEsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fstCopy_Proj5.Controllers
+{
+    public class DATEsController : Controller
+    {
+        // GET: DATEs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
